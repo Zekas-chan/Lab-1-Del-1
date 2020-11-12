beb6e7f689538c3f79d8cd56be5e8326343307d8
Zekas-chan <philar-9@student.ltu.se>
2020-11-12 16:36:09 +0100

    Kan nu generera grafer och Adjacency lists!

--- Lab1Del1/src/findMinimumSpanningTree.java
+++ Lab1Del1/src/findMinimumSpanningTree.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Random;
+import java.util.Vector;
 
 /**
  * 
@@ -15,9 +16,10 @@ import java.util.Random;
  * Få en adjacency list i någon form
  */
 public class findMinimumSpanningTree {
-	private Node root;
 	private Node[] nodesGenerated;
-	private Random r;
+	public Random r;
+//	private Node root;
+//	private Random r;
 	
 	/*
 	 * Tanken är att denna klass agerar som det kanter går mellan; som jag tolkat specen har noder i sig ingen vikt.
@@ -25,20 +27,11 @@ public class findMinimumSpanningTree {
 	 */
 	private class Node{
 		String identifier;
-		Edge[] edges;
+		Vector<Edge> edges;
 		
 		Node(String identifier, int maxEdges){
 			this.identifier = identifier;
-			edges = new Edge[maxEdges-1];
-		}
-		
-		void addEdge(Edge e) {
-			for(int i = 0; i < edges.length; i++) {
-				if(edges[i] == null) {
-					edges[i] = e;
-					break;
-				}
-			}
+			edges = new Vector<Edge>(maxEdges-1);
 		}
 		
 		public String toString() {
@@ -53,11 +46,24 @@ public class findMinimumSpanningTree {
 		Node Node1, Node2;
 		int weight;
 		
+		/*
+		 * Konstruktor
+		 */
 		Edge(Node node1, Node node2){
-			this.Node1 = node1;
-			this.Node2 = node2;
-			this.weight = -1;
+			if(node1.edges.capacity() > node1.edges.size() && node2.edges.capacity() > node2.edges.size()) {
+				this.Node1 = node1;
+				this.Node2 = node2;
+				node1.edges.add(this);
+				node2.edges.add(this);
+				this.weight = 1 + r.nextInt(10);
+			}
 		}
+		
+		Node getOtherNode(Node origin) {
+			return origin.toString() == Node1.toString() ? Node2 : Node1;
+		}
+		
+		
 	}
 	
 	
@@ -66,7 +72,7 @@ public class findMinimumSpanningTree {
 	 */
 	private class assignIdentifier {
 		private int n;
-		private String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"}; //+resten av alfabetet, senare
+		private String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 		
 		assignIdentifier(){
 			n = -1;
@@ -74,124 +80,194 @@ public class findMinimumSpanningTree {
 		
 		String getIdentifier() {
 			n++;
+			//System.out.println("Generated node "+letters[n]);
 			return letters[n];
 		}
 	}
 	
-	
-	public findMinimumSpanningTree() {
-		/*
-		 * Konstruktorn bör generera en ny graf när den körs?
-		 */
-		
-		//Generar en graf
+	/*
+	 * Konstruktorn bör generera en ny graf när den körs?
+	 */
+	public void generateGraph() {
+		//Skapar en intern klass som tilldelar identifierare
 		assignIdentifier assigner = new assignIdentifier();
+		
+		//Skapar en Random för att slumpa fram antalet noder
 		Random r = new Random(System.currentTimeMillis());
 		
+		//Antalet noder som ska genereras
 		int totalNodesToMake = 4 + r.nextInt(4); //ska vara 23
 		
+		//Tilldelar nodesGenerated en array med storlek motsvarande antalet noder som ska skapas
 		nodesGenerated = new Node[totalNodesToMake];
 		
+		//Fyller arrayen med noder
 		for(int i = 0; i < totalNodesToMake; i++) {
 			nodesGenerated[i] = new Node(assigner.getIdentifier(), totalNodesToMake);
 		}
 		
-		while(!graphIsConnected(nodesGenerated)) {
+		//Tilldelar kanter till noderna tills dess att alla noder kan nås från en nod
+		do{
 			Node node1;
 			Node node2;
 			while(true) {
 				node1 = nodesGenerated[r.nextInt(nodesGenerated.length)];
 				node2 = nodesGenerated[r.nextInt(nodesGenerated.length)];
-				if(node1 != node2) {
+				if(node1.toString() != node2.toString() && edgeAlreadyPresent(node1, node2)) {
 					break;
 				}
 			}
 			Edge e = new Edge(node1, node2);
-			node1.addEdge(e);
-			node2.addEdge(e);
+			System.out.println("Generated edge has weight "+e.weight);
 			
-		}
+		}while(!isConnected());
 	}
-
-
+	
 	/*
-	 * Ska returnera true när alla noder i grafen kan nås från alla andra noder.
+	 * Hjälpmetod för generateGraph().
 	 */
-	boolean graphIsConnected(Node[] nodes) {
-		//variables
-		Node[] markedNodes = new Node[nodes.length];
-		LinkedList<Node> fifo = new LinkedList<Node>();
-		
-		
-		//spaghetti begins here
-		fifo.add(nodes[0]);
-		
-		while (!fifo.isEmpty()) {
-			int markedIterator = 0;
-			Node s = fifo.pop();
-			for(int i = 0; i < markedNodes.length; i++) {
-				if(s == markedNodes[i]) {
-					break;
-				}else {
-					//System.out.println("List length: "+markedNodes.length+", markedIterator: "+markedIterator+", current value of i: "+i);
-					markedNodes[markedIterator] = s;
-					markedIterator++;
-					for(int x = 0; x < s.edges.length; x++) {
-						if(s.edges[x] == null) {
-							continue;
-						}
-						Node nodeToAdd = s.edges[x].Node1 == s ? s.edges[x].Node2 : s.edges[x].Node1;
-						fifo.add(nodeToAdd);
-					}//for
-				}//if
-			}//for
-			//System.out.println("amount of whiles");
-		}//while
-		
-		//if an element is null, all nodes were not marked
-		for(int i = 0; i < markedNodes.length; i++) {
-			System.out.println(markedNodes[i].toString());
-			if(markedNodes[i] == null) {
-				
+	private boolean edgeAlreadyPresent(Node n1, Node n2) {
+		for(int i = 0; i < n1.edges.size(); i++) {
+			if(n1.edges.get(i).getOtherNode(n1).toString() == n2.toString()){
 				return false;
 			}
 		}
 		return true;
 	}
 	
-//	private int findNextNull(Node[] list) {
-//		for(int i = 0; i < list.length; i++) {
-//			if(list[i] == null) {
-//				return i;
-//			}
-//		}
-//		
-//	}
+	public findMinimumSpanningTree() {
+		this.r = new Random(System.currentTimeMillis());
+	}
+	
+	
+	/*
+	 * Hannas isConnected.
+	 */
+	boolean isConnected() {
+        LinkedList<Node> markedNodes = new LinkedList<Node>();
+        LinkedList<Node> Q = new LinkedList<Node>();
+        Q.add(nodesGenerated[0]);
+        while (!Q.isEmpty()) {
+            Node u = Q.pop();
+            if(!isInLst(u, markedNodes)) {
+                markedNodes.add(u);
+                for (int i = 0; i < u.edges.size(); i++) {
+                    Node nodeToAdd = u.edges.get(i).Node1 == u ? u.edges.get(i).Node2 : u.edges.get(i).Node1;
+                    Q.add(nodeToAdd);
+                }
+            }
+
+
+
+        }
+        if (markedNodes.size() == nodesGenerated.length) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean isInLst(Node node,LinkedList<Node> nodeLst) {
+        for(int i = 0; i < nodeLst.size(); i++) {
+            if (node == nodeLst.get(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
 	
 	/*
 	 * Skapar en jämn matris för alla noder i grafen
 	 */
-//	int[][] generateAdjacencyMatrix() {
-//		
-//	}
+	int[][] generateAdjacencyMatrix() {
+		
+	}
 	
 	
 	/*
-	 * Skapar en ojämn lista för alla noder i grafen
+	 * Skapar en "ojämn" lista för alla noder i grafen
 	 */
-//	Node[][] generateAdjancencyList(){
-//		
-//	}
+	String generateAdjacencyList(){
+		String Lst = "\nGraph adjacency list:\n";
+		for(int i = 0; i < nodesGenerated.length; i++) {
+			Lst += nodesGenerated[i].toString()+":";
+			for(int x = 0; x < nodesGenerated[i].edges.size(); x++) {
+				Lst = Lst + nodesGenerated[i].edges.get(x).getOtherNode(nodesGenerated[i]).toString();
+			}
+			Lst += "\n";
+		}
+		return Lst;
+	}
 	
+    
+    //testfunktioner, ignore =====================================================================
+    private void jumpingThroughHoops() {
+    	Node a = new Node("a", 3);
+    	Node b = new Node("b", 3);
+    	Edge e = new Edge(a, b);
+    	
+    	System.out.println(e.getOtherNode(b).toString());
+    }
+    
+	/*
+	 * Testgraf med känt utseende.
+	 * Modellerad efter https://gyazo.com/67ff3e7cf619dc77a287dff14d473672
+	 * En viktig ändring: Dubbla kanter tillåts inte, grafen ska tolkas som oriktad
+	 */
+	private Node[] knownGraph() {
+		Node v1 = new Node("a", 4); //v1
+		Node v2 = new Node("b", 4); //v2
+		Node v3 = new Node("c", 4); //v3
+		Node v4 = new Node("d", 4); //v4
+		Node v5 = new Node("e", 4); //v5
+		
+		//Kanter för nod v1
+		Edge v1_v2 = new Edge(v1, v2);
+		Edge v1_v5 = new Edge(v1, v5);
+		
+		//Kanter för nod v2
+		Edge v2_v1 = new Edge(v2, v1);
+		Edge v2_v5 = new Edge(v2, v5);
+		Edge v2_v3 = new Edge(v2, v3);
+		Edge v2_v4 = new Edge(v2, v4);
+		
+		//Kanter för nod v3
+		Edge v3_v2 = new Edge(v3, v2);
+		Edge v3_v4 = new Edge(v3, v4);
+		Edge v3_v5 = new Edge(v3, v5);
+		
+		//Kanter för nod v4
+		Edge v4_v2 = new Edge(v4, v2);
+		Edge v4_v3 = new Edge(v4, v3);
+		Edge v4_v5 = new Edge(v4, v5);
+		
+		//Kanter för nod v5
+		Edge v5_v1 = new Edge(v5, v2);
+		Edge v5_v2 = new Edge(v5, v5);
+		Edge v5_v3 = new Edge(v5, v2);
+		Edge v5_v4 = new Edge(v5, v5);
+		
+		Node [] graph = {v1, v2, v3, v4, v5};
+		return graph;
+	}
+	
+	//end testfunktioner ========================================================================
 	public static void main(String[] args) {
 		findMinimumSpanningTree asdf = new findMinimumSpanningTree();
+		
+		asdf.generateGraph();
+		
+		for(int i = 0; i < asdf.nodesGenerated.length; i++) {
+			System.out.println("Node "+asdf.nodesGenerated[i].toString()+" has "+asdf.nodesGenerated[i].edges.size()+" edges.");
+		}
+		
 		//asdf.generateAdjacencyMatrix()
 		
 		//much debug
-		for(int i = 0; i < asdf.nodesGenerated.length; i++) {
-			//System.out.println("Node "+(i+1)+" is: "+asdf.nodesGenerated[i].toString()+"\n");
-		}
+		System.out.print(asdf.generateAdjacencyList());
 	}
+	
+	
+	
 //==========================================================================================
 	/*
 	 * (Relevant för del 2)
